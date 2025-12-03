dc97132194d8fbdb9d706b15481061772c289b24
chandu736 <kaushikponnaganti@gmail.com>
2025-12-03 22:08:45 +0530

    updated next.config.js file

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     output: "export",
+    trailingSlash: true,
     images: {
         unoptimized: true,
         domains: ['tx-hospital-blog-images.s3.ap-south-2.amazonaws.com', 'img.youtube.com', 'flagcdn.com'],
