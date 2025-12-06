672612a63e82b4dc76ec740b09fa2f9e2ac72bea
Tharun Bejawada <tharunbejawada@Tharuns-MacBook-Air.local>
2025-12-06 14:51:29 +0530

    Test Changes for Amplify deployment

--- next.config.mjs
+++ next.config.mjs
@@ -1,23 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    // output: "export",
-    // trailingSlash: true,
-    // images: {
-    //     unoptimized: true,
-    //     domains: ['tx-hospital-blog-images.s3.ap-south-2.amazonaws.com', 'img.youtube.com', 'flagcdn.com'],
-    // },
-  
   eslint: {
-    // Warning: This allows production builds to successfully complete even if
-    // your project has ESLint errors.
     ignoreDuringBuilds: true,
   },
   typescript: {
-    // !! WARN !!
-    // Dangerously allow production builds to successfully complete even if
-    // your project has type errors.
     ignoreBuildErrors: true,
   },
-}
+};
 
-module.exports = nextConfig
+// ERROR WAS HERE: change "module.exports = nextConfig" to:
+export default nextConfig;
