2f0de7104406e28bafbd4ead399b33758ac08445
Tharun Kumar Bejawada <59820020+TharunBejawada@users.noreply.github.com>
2025-12-06 14:24:50 +0530

    Disable Next.js configuration options
    Comment out output, trailingSlash, and images settings.

--- next.config.mjs
+++ next.config.mjs
@@ -1,11 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    output: "export",
-    trailingSlash: true,
-    images: {
-        unoptimized: true,
-        domains: ['tx-hospital-blog-images.s3.ap-south-2.amazonaws.com', 'img.youtube.com', 'flagcdn.com'],
-    },
+    // output: "export",
+    // trailingSlash: true,
+    // images: {
+    //     unoptimized: true,
+    //     domains: ['tx-hospital-blog-images.s3.ap-south-2.amazonaws.com', 'img.youtube.com', 'flagcdn.com'],
+    // },
 };
 
 export default nextConfig;
