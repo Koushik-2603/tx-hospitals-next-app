811fab54c1998f4cc1ef9b089875a581a17887c4
chandu736 <kaushikponnaganti@gmail.com>
2026-05-07 22:41:16 +0530

    Added google tag

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -4,6 +4,22 @@ export default function Document() {
     return (
         <Html lang="en">
             <Head>
+                {/* Google tag (gtag.js) */}
+                <script
+                    async
+                    src="https://www.googletagmanager.com/gtag/js?id=AW-18108940602"
+                ></script>
+                <script
+                    dangerouslySetInnerHTML={{
+                        __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', 'AW-18108940602');
+            `,
+                    }}
+                ></script>
+
                 {/* Favicon */}
                 <link rel="icon" href="/TX-favicon.png" />
                 <link rel="apple-touch-icon" href="/TX-favicon.png" />
